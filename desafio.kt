592cd8daecabfa0c928583fd4306eb88342fa78c
Simone Ramos <124322665+simoneRamosDev@users.noreply.github.com>
2024-02-29 09:25:57 -0300

    projetoKotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,30 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 365)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.hashCode()} matriculado na formação $nome")
     }
 }
-
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+    
+    val formacao = Formacao("Desenvolvimento de Software", listOf(conteudo1, conteudo2))
+    
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
 }
+
